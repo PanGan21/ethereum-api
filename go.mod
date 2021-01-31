76cbee6969c4fd7aee867058a805719c5e961a58
PanGan21 <ganelis.pan@gmail.com>
2021-01-31 19:21:29 +0100

    add handlers and main

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.15
 
 require (
 	github.com/btcsuite/btcd v0.21.0-beta // indirect
-	github.com/ethereum/go-ethereum v1.9.25 // indirect
+	github.com/ethereum/go-ethereum v1.9.25
+	github.com/gorilla/mux v1.8.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 )
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	Handlers "github.com/PanGan21/ethereum-api/handler"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Create a client instance to connect to the provider
+	client, err := ethclient.Dial("http://localhost:8545")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create a mux router
+	r := mux.NewRouter()
+
+	// Define a single endpoint
+	r.Handle("/api/eth/{module}", Handlers.ClientHandler{client})
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
