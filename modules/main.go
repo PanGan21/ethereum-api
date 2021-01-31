19a198647e5f41bd774dec3e3651ec6aeb105550
PanGan21 <ganelis.pan@gmail.com>
2021-01-31 12:43:20 +0100

    get latest block

---
+++ modules/main.go
@@ -0,0 +1,53 @@
+package modules
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	Models "github.com/PanGan21/ethereum-api/models"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+// GetLatestBlock from blockchain
+func GetLatestBlock(client ethclient.Client) *Models.Block {
+	// Recover from panics
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	// Query the latest block
+	header, _ := client.HeaderByNumber(context.Background(), nil)
+	blockNumber := big.NewInt(header.Number.Int64())
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Build the response with our model
+	_block := &Models.Block{
+		BlockNumber:       block.Number().Int64(),
+		Timestamp:         block.Time(),
+		Difficulty:        block.Difficulty().Uint64(),
+		Hash:              block.Hash().String(),
+		TransactionsCount: len(block.Transactions()),
+		Transactions:      []Models.Transaction{},
+	}
+
+	// Transactions
+	for _, tx := range block.Transactions() {
+		_block.Transactions = append(_block.Transactions, Models.Transaction{
+			Hash:     tx.Hash().String(),
+			Value:    tx.Value().String(),
+			Gas:      tx.Gas(),
+			GasPrice: tx.GasPrice().Uint64(),
+			Nonce:    tx.Nonce(),
+			To:       tx.To().String(),
+		})
+	}
+	return _block
+}
